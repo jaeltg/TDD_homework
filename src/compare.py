d7561364e8e96827f2319ca11b6c6ce1403e837c
jaeltg <jaeltg@hotmail.com>
2021-03-03 19:43:03 +0000

    initial commit

---
+++ src/compare.py
@@ -0,0 +1,7 @@
+def compare(first_number, second_number):
+    if first_number > second_number:
+        return f"{first_number} is greater than {second_number}"
+    elif first_number < second_number:
+         return f"{first_number} is less than {second_number}"
+    else:
+        return "numbers are the same"       
